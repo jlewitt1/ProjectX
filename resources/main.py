127a19d676fe87335ef160eb4900e2fbe9890ece
Ishay Haykeen <haykeen@usc.edu>
2018-06-20 19:32:16 +0300

    ishay commit

--- resources/main.py
+++ resources/main.py
@@ -4,6 +4,8 @@ This is the server side for GreenPath
 
 from bottle import route, run, get, template, static_file, request, response, debug
 import json
+from src.user import User
+
 
 @get("/")
 def index():
@@ -13,21 +15,21 @@ def index():
 @route("/newroute", method='POST')
 def handle_route_request():
     user_preferences = request.POST.get('userPreferences')
+    user = User(user_preferences)
+    return json.dumps(user.process_user_setting(user_preferences))  # returns to js
 
-    return json.dumps(process_user_setting(user_preferences))  # returns to js
 
-
-@route('/resources/js/<filename:re:.*\.js>', method='GET')
+@route('/js/<filename:re:.*\.js>', method='GET')
 def javascripts(filename):
     return static_file(filename, root='js')
 
 
-@route('/resources/css/<filename:re:.*\.css>', method='GET')
+@route('/css/<filename:re:.*\.css>', method='GET')
 def stylesheets(filename):
     return static_file(filename, root='css')
 
 
-@route('/resources/images/<filename:re:.*\.(jpg|png|gif|ico)>', method='GET')
+@route('/images/<filename:re:.*\.(jpg|png|gif|ico)>', method='GET')
 def images(filename):
     return static_file(filename, root='images')
 
