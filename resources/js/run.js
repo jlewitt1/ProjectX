1cad9fb1d62ccc0d649fd8571d65c45d70c7ecdc
Barak Fisher <barakfisher@gmail.com>
2018-06-21 13:19:23 +0300

    login added

--- resources/js/run.js
+++ resources/js/run.js
@@ -20,6 +20,14 @@
 
 
 //    #################3run JSON
-$('#range').on("change", function() {
-    $('.output').val(this.value +" Km" );
-    }).trigger("change");
+$('#range').on("change", function () {
+    $('.output').val(this.value + " Km");
+}).trigger("change");
+
+$('.log-in').on("click", function () {
+    $('.login-screen').css('display', 'flex');
+});
+
+function hide(){
+    $('.login-screen').css('display', 'none');
+}
